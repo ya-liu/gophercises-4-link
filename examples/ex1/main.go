22ab01b5b2fe788f5e3eb7413d61ecbac6b92128
Ya Liu <sarahl0216@gmail.com>
2022-04-17 22:11:15 -0700

    add example folders and example 1 main file

---
+++ examples/ex1/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	link "github.com/ya-liu/gophercises-4-link"
+)
+
+var exampleHtml = `
+<html>
+<body>
+  <h1>Hello!</h1>
+  <a href="/other-page">A link to another page</a>
+</body>
+</html>
+`
+
+// Create a reader out of a string
+func main() {
+	r := strings.NewReader(exampleHtml)
+	links, err := link.Parse(r)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%+v\n", links)
+}
