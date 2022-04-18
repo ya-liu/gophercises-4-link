cf0d257b8fc1329ddb1ead7b069268e1793375db
Ya Liu <sarahl0216@gmail.com>
2022-04-17 22:32:14 -0700

    printing out test nodes for visualization

--- parse.go
+++ parse.go
@@ -1,6 +1,11 @@
 package link
 
-import "io"
+import (
+	"fmt"
+	"io"
+
+	"golang.org/x/net/html"
+)
 
 // Link represents a link in an HTML document
 type Link struct {
@@ -10,6 +15,23 @@ type Link struct {
 
 // Parse will take an HTML document and will return a slice of links parsed from it
 func Parse(r io.Reader) ([]Link, error) {
-	var link []Link
-	return link, nil
+	doc, err := html.Parse(r)
+	if err != nil {
+		return nil, err
+	}
+
+	dfs(doc, "")
+	// var link []Link
+	return nil, nil
+}
+
+func dfs(n *html.Node, padding string) {
+	msg := n.Data
+	if n.Type == html.ElementNode {
+		msg = "<" + msg + ">"
+	}
+	fmt.Println(padding, msg)
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		dfs(c, padding+"  ")
+	}
 }
